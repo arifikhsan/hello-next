3e2f0704483a574c2040fa248dcfd55da948c26a
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-12-31 10:26:48 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello Next.js</h1>
+      <p style={{ color: 'skyblue' }}>Styling that really quick</p>
+      <footer>Lets go</footer>
+    </div>
+  );
+};
+
+export default Index;
